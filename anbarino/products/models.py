f12e1f5649281d5304b9c469088c4c8bd1eb647f
mamad <137838327+rootmamad@users.noreply.github.com>
2025-06-14 16:47:15 +0330

    6

--- anbarino/products/models.py
+++ anbarino/products/models.py
@@ -1,13 +1,16 @@
+from datetime import datetime
+
 from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
     price = models.PositiveIntegerField(default=0)
     quantity = models.PositiveIntegerField(default=0)
     purchased_quantity = models.PositiveIntegerField(default=0)
     returned_quantity = models.PositiveIntegerField(default=0)
     damaged_quantity = models.PositiveIntegerField(default=0)
-
+    created_at = models.DateTimeField(auto_now_add=True)
     def update_status(self):
         self.quantity = self.purchased_quantity + self.returned_quantity - self.purchased_quantity
         self.save()
--- anbarino/products/views.py
+++ anbarino/products/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def view(request):
-    return render(request, 'products/index.html')
+    products = Product.objects.all()
+    ctx = {
+        'best_selling_products': Product.objects.order_by('-purchased_quantity')[:10],
+        'new_products': Product.objects.order_by('-created_at')[:10],
+        'low_stock_products': Product.objects.filter(quantity__lt=10)[:10],
+        'bulk_products': Product.objects.filter(quantity__gte=100)[:10],
+    }
+
+    return render(request, 'products/index.html', ctx)#{'products': products})
