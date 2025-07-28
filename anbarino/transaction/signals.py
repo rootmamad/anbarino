a2faeb97546e891e20a8b1b3302e3685f595f197
mamad <137838327+rootmamad@users.noreply.github.com>
2025-07-28 11:44:08 +0330

    14

--- anbarino/transaction/signals.py
+++ anbarino/transaction/signals.py
@@ -7,40 +7,45 @@ from django.core.exceptions import ValidationError
 
 @receiver(post_save, sender=Transaction)
 def update_product_status(sender, instance, **kwargs):
+    if not instance.is_approved:
+        return
 
-    if instance.is_approved:
-        product_status, created = Product.objects.get_or_create(name=instance.product)
-        user_balance = UserBalance.objects.get(user=instance.user)
+    product = instance.product
+    user_balance = UserBalance.objects.get(user=instance.user)
 
-        if instance.transaction_type == Transaction.PURCHASE:
-            product_status.purchased_quantity += instance.quantity
-            product_status.quantity -= instance.quantity
-            if not user_balance.deduct_balance(instance.quantity * instance.product.price):
-                raise ValidationError("موجودی کاربر کافی نیست!")
-        elif instance.transaction_type == Transaction.RETURNED:
-            total_purchased = Transaction.objects.filter(
-                user=instance.user, product=instance.product, transaction_type=Transaction.PURCHASE
-            ).aggregate(models.Sum('quantity'))['quantity__sum'] or 0
+    total_purchased = Transaction.objects.filter(
+        user=instance.user, product=product, transaction_type=Transaction.PURCHASE, is_approved=True
+    ).aggregate(models.Sum('quantity'))['quantity__sum'] or 0
 
-            if instance.quantity > total_purchased:
-                raise ValueError(f"{instance.user.username} نمی‌تواند بیش از مقدار خریداری‌شده مرجوع کند!")
+    total_returned = Transaction.objects.filter(
+        user=instance.user, product=product, transaction_type=Transaction.RETURNED, is_approved=True
+    ).aggregate(models.Sum('quantity'))['quantity__sum'] or 0
 
-            product_status.returned_quantity += instance.quantity
-            product_status.quantity += instance.quantity
-            user_balance.balance += instance.quantity * instance.product.price
-            user_balance.save()
-        elif instance.transaction_type == Transaction.DAMAGED:
-            total_purchased = Transaction.objects.filter(
-                user=instance.user, product=instance.product, transaction_type=Transaction.PURCHASE
-            ).aggregate(models.Sum('quantity'))['quantity__sum'] or 0
+    total_damaged = Transaction.objects.filter(
+        user=instance.user, product=product, transaction_type=Transaction.DAMAGED, is_approved=True
+    ).aggregate(models.Sum('quantity'))['quantity__sum'] or 0
 
-            if instance.quantity > total_purchased:
-                raise ValueError(f"{instance.user.username} نمی‌تواند بیش از مقدار خریداری‌شده خرابی ثبت کند!")
+    if instance.transaction_type == Transaction.RETURNED:
+        if total_returned + instance.quantity > total_purchased - total_damaged:
+            raise ValidationError("نمیتوان بیشتر از مقدار قابل برگشت مرجوع کرد.")
+        product.returned_quantity += instance.quantity
+        product.quantity += instance.quantity
+        user_balance.balance += instance.quantity * product.price
+        user_balance.save()
 
-            product_status.damaged_quantity += instance.quantity
-            #product_status.quantity -= instance.quantity
+    elif instance.transaction_type == Transaction.DAMAGED:
+        if total_damaged + instance.quantity > total_purchased - total_returned:
+            raise ValidationError("نمیتوان بیشتر از مقدار قابل خرابی ثبت کرد.")
+        product.damaged_quantity += instance.quantity
 
-        product_status.save()
+    elif instance.transaction_type == Transaction.PURCHASE:
+        if not user_balance.deduct_balance(instance.quantity * product.price):
+            raise ValidationError("موجودی کاربر کافی نیست!")
+        product.purchased_quantity += instance.quantity
+        product.quantity -= instance.quantity
+
+    product.save()
+
 
 @receiver(post_delete, sender=Transaction)
 def revert_product_status(sender, instance, **kwargs):
